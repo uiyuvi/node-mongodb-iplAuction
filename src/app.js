2d8ca6ad41f750c2a487051d1d88e2ac912b9421
yuvaraj <yuvaa.it@gmail.com>
2024-02-03 17:40:48 +0100

    Perform login , add token to Admin

--- src/app.js
+++ src/app.js
@@ -1,13 +1,39 @@
 const express = require("express");
 const adminRouter = require("./routers/admin");
 require("./mongoose/db/mongoose");
-
+const Admin = require('./mongoose/models/admin');
+const jwt = require("jsonwebtoken");
 //setting up express router
 const app = express();
 
 //setting up the express middlewares
 app.use(express.json());
 app.use(adminRouter);
+
+app.post('/login', async (req, res) => {
+    const { name, password } = req.body;
+
+    try {
+        const admin = await Admin.findOne({ name });
+        if (!admin || password !== admin.password) {
+            return res.status(400).json({ message: 'Invalid credentials.' });
+        }
+
+        // Generate JWT token
+        const token = jwt.sign({ _id : admin._id }, 'xeEo2M0ol8CeWr7Nw2g2GjH8QEUK4dyyKCHi4TYJK6znm5fuAHIIPHSQ5YvdVcLlnaxppN64xK6xbhRileWvIlzCEqrBMCiITD8z', {
+            expiresIn: '1h', // Set token expiration time
+        });
+
+        // Update admin's tokens array
+        admin.tokens.push({ token });
+        await admin.save();
+
+        res.json({ token });
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ message: 'Server error.' });
+    }
+});
 
 //exporting the app
 module.exports = app;
